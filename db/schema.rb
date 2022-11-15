745fe26d6ad1d9b1b3db630770f85cffc76744e5
Oigen <ekonvissar@gmail.com>
2022-11-15 16:47:23 +0300

    Error-handing

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_110707) do
-  create_table "quastions", force: :cascade do |t|
-    t.string "title"
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_093030) do
+  create_table "answers", force: :cascade do |t|
     t.text "body"
+    t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
+
+
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -25,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_110707) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
