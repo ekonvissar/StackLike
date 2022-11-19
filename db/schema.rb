4b393e67a5ea43ae45bca02115a24bdf33a5c76b
Oigen <ekonvissar@gmail.com>
2022-11-19 17:40:12 +0300

    Registration + Pagy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_093030) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_121504) do
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.integer "question_id", null: false
@@ -19,13 +19,27 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_093030) do
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-
+  create_table "quastions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
